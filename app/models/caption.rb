63546b3c00f6d96d9fe22a93fe29732c9bdb67ad
Jesse Cai <mojacc@gmail.com>
2015-10-24 17:41:00 +0800

    Modify image processor and move it to services.

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -1,2 +1,5 @@
 class Caption < ActiveRecord::Base
+  def text
+    line2.blank? ? line1 : line1 + "\n" + line2
+  end
 end
---
+++ app/services/image_processor.rb
@@ -0,0 +1,30 @@
+require "rmagick"
+require "tempfile"
+
+class ImageProcessor
+  def self.caption_image(base_image, caption, user=nil)
+    img = Magick::Image.read(base_image.image.url).first
+
+    draw = Magick::Draw.new
+    draw.align = Magick::CenterAlign
+    draw.annotate(img, 512, 512, 256, 450, caption.text) do
+      self.font = 'public/uploads/SimHei.ttf'
+      self.pointsize = 48
+      self.font_weight = Magick::BoldWeight
+      self.fill = 'black'
+      self.gravity = Magick::SouthEastGravity
+      self.stroke = "white"
+    end
+
+    captioned = CaptionedImage.new(user: user)
+    tempfile = Tempfile.new(["processed_image", ".gif"])
+    img.write(tempfile.path)
+
+    uploader = ImageUploader.new
+    uploader.store!(tempfile)
+
+    captioned.image = uploader
+
+    return captioned
+  end
+end
